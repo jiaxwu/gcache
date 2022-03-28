53241d45d2128215758616c765b4cac5605c38dd
xhsf <827032783@qq.com>
2022-03-28 21:12:57 +0800

    实现基于ETCD的服务注册和服务

---
+++ test/naming/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jiaxwu/gcache/naming"
+	"log"
+)
+
+func main() {
+	n, err := naming.New("gcache/", []string{"49.233.30.197:2379"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	watch := n.Watch(context.Background())
+	fmt.Println(n.GetAddrs(context.Background()))
+	if err := n.Register(context.Background(), "localhost:8081"); err != nil {
+		log.Fatalln(err)
+	}
+	for event := range watch {
+		fmt.Println(event)
+	}
+}
